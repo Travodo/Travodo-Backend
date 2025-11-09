4658c245690c0a7fbbd182a4d97049be516f336e
Jack <devjack050700@gmail.com>
2025-11-10 00:25:22 +0900

    프로젝트 생성

---
+++ src/main/java/gdg/travodobackend/TravodoBackendApplication.java
@@ -0,0 +1,13 @@
+package gdg.travodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravodoBackendApplication.class, args);
+    }
+
+}
