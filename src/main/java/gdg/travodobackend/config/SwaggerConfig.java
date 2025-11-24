155f321d0f7d94c1ea363f4a5a279ecb36567e05
Jack <devjack050700@gmail.com>
2025-11-24 19:10:26 +0900

    feat/#10: 로컬, 배포환경 swagger 구분

--- src/main/java/gdg/travodobackend/config/SwaggerConfig.java
+++ src/main/java/gdg/travodobackend/config/SwaggerConfig.java
@@ -8,13 +8,18 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${spring.profiles.active:local}")
+    private String activeProfile;
 
     @Bean
     public OpenAPI openAPI() {
@@ -29,10 +34,26 @@ public class SwaggerConfig {
         SecurityRequirement securityRequirement = new SecurityRequirement()
                 .addList("bearerAuth");
 
-        // 서버 정보 설정
-        Server prodServer = new Server()
-                .url("https://travodo.duckdns.org")
-                .description("운영 서버");
+        // 서버 정보 설정 (환경별로 다르게 설정)
+        List<Server> servers = new ArrayList<>();
+        
+        if ("local".equals(activeProfile)) {
+            // 로컬 환경: 로컬 서버와 운영 서버 모두 제공
+            Server localServer = new Server()
+                    .url("http://localhost:8080")
+                    .description("로컬 개발 서버");
+            Server prodServer = new Server()
+                    .url("https://travodo.duckdns.org")
+                    .description("운영 서버");
+            servers.add(localServer);
+            servers.add(prodServer);
+        } else {
+            // 프로덕션 환경: 운영 서버만 제공
+            Server prodServer = new Server()
+                    .url("https://travodo.duckdns.org")
+                    .description("운영 서버");
+            servers.add(prodServer);
+        }
 
         // API 정보 설정
         Info info = new Info()
@@ -49,7 +70,7 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .info(info)
-                .servers(List.of(prodServer))
+                .servers(servers)
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", securityScheme))
                 .addSecurityItem(securityRequirement);
